50ea6ec930066f875fa26e6ff54956e2bc6fa1d3
DieselMeister <gitkraken@dieselmeister.com>
2020-06-13 18:40:23 +0200

    wn

--- PerryRhodan.AudiobookPlayer/WhatsNew.fs
+++ PerryRhodan.AudiobookPlayer/WhatsNew.fs
@@ -18,13 +18,14 @@ Was gibt neues in dieser Version:
 
 Downloads laufen jetzt in einem Hintergrundprozess, damit soll verhindert werden, dass wenn das Telefon die App automatisch beendet, der Download abbricht.
 
+
 - Anpassung der Titelerkennung, sowie Reperatur der Titel:
 
 Wichtig! Damit die Titel "repariert" werden, müssen unter "Browse" einmal die Titel aktualisiert werden.
 
-Da ich als Entwickler noch keine direkte Schnittstelle zu Einsamedien haben, muss ich die Titel und Gruppen automatisch anhand der Einträge auf der Shop-Download-Seite erkennen.
-Wie ihr sicher verstehen, kann es so oftmal zu Fehlern in der Erkennung kommen. Ich habe einige Fälle jetzt angepasst. Darunter Warhammer 40k, die Perry Rhodan Storys und Mission SOL 2. (Danke Thomas)
-Ich bin dabei auf eurte Mithilfe angewiesen, denn ich habe nicht alle Hörbücher, die es gibt. Wenn ihr was seht, schreibt mir eine Nachricht unter Support-Feedback oder an info@hardt-solutions.de. Danke
+Da ich als Entwickler noch keine direkte Schnittstelle zu Einsamedien habe, muss ich die Titel und Gruppen automatisch anhand der Einträge auf der Shop-Download-Seite erkennen.
+Wie ihr sicher versteht, kann es so oftmal zu Fehlern in der Erkennung kommen. Denn die Einträge sind ja nicht immer identisch in ihrer Form. Ich habe einige Fälle jetzt angepasst. Darunter Warhammer 40k, die Perry Rhodan Storys und Mission SOL 2. (Danke Thomas)
+Ich bin dabei auf eure Mithilfe angewiesen, denn ich habe nicht alle Hörbücher, die es gibt. Wenn ihr was seht, schreibt mir eine Nachricht unter Support-Feedback oder an info@hardt-solutions.de. Danke
 
 
 - Gesamtfortschitt im Hörbuch:
@@ -32,9 +33,11 @@ Ich bin dabei auf eurte Mithilfe angewiesen, denn ich habe nicht alle Hörbüche
 Wieviel euch noch von dem Hörbuch verbleibt, wird jetzt auch angezeigt. Einmal als kleine "Torte" links im Hörbuch-Bild oder eben als Text unter dem Titel.
 Außerdem noch auf der Player-Seite.
 
+
 - Design-Anapssungen:
 
 Auf der Playerseite ist jetzt das Cover mehr "präsent". Dazu noch ein paar andere Kleinigkeiten.
+
 
 - Bugfixes:
 
