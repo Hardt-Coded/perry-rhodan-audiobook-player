dbde66ab963400a11438408368d3012378f5c348
Daniel Hardt <info@hardt-solutions.de>
2022-06-23 18:53:49 +0200

    global

--- PerryRhodan.AudiobookPlayer/Global.fs
+++ PerryRhodan.AudiobookPlayer/Global.fs
@@ -1,7 +1,7 @@
 ﻿module Global
 
-    let appcenterAndroidId = "e806b20e-0e4c-4209-81c1-9ff48478f932"
-    let supportMessageApi = "https://prod-86.westeurope.logic.azure.com:443/workflows/0a2e452eb2aa4546bb6ce267e7cb3c28/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=0DtXE6NS5QzYoAqH1_vuQfhIvg48VqGp16st4tS55fg"
+    let appcenterAndroidId = ""
+    let supportMessageApi = ""
     let messageEndpoint = "https://einsamedienappmessages.z1.web.core.windows.net/messages.json"
 
 
