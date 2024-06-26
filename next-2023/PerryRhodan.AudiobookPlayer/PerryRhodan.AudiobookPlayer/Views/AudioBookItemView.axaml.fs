ffc10505bf48bbe89aa2a3b711971fa5505bd113
Daniel Hardt <daniel@hardt-solutions.com>
2024-06-26 17:21:41 +0200

    next version

---
+++ next-2023/PerryRhodan.AudiobookPlayer/PerryRhodan.AudiobookPlayer/Views/AudioBookItemView.axaml.fs
@@ -0,0 +1,13 @@
+namespace PerryRhodan.AudiobookPlayer.Views
+
+open Avalonia
+open Avalonia.Controls
+open Avalonia.Markup.Xaml
+
+type AudioBookItemView () as this = 
+    inherit UserControl ()
+
+    do this.InitializeComponent()
+
+    member private this.InitializeComponent() =
+        AvaloniaXamlLoader.Load(this)
